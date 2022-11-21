75202d7628095ae02fa142e83d4f6b1975ab4b0d
skinsey204 <111942530+skinsey204@users.noreply.github.com>
2022-11-21 12:56:48 -0600

    Update inbox.js

--- mail/static/inbox.js
+++ mail/static/inbox.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').addEventListener('submit', add_sent);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +32,8 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function add_sent(event) {
+  
 }
