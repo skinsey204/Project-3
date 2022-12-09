4951d2c8abfb7670023379d2e11f554ab4cd7023
skinsey204 <111942530+skinsey204@users.noreply.github.com>
2022-12-09 12:59:03 -0600

    Update inbox.js

--- mail/static/inbox.js
+++ mail/static/inbox.js
@@ -18,6 +18,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -30,12 +31,46 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch('/emails/' + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+    
+    emails.forEach(email => {
+      var div = document.createElement('div');
+      if(email['read']) {
+        div.className = "read_email"
+      } else {
+        div.className = "unread_email"
+      }
+      div.innerHTML = `
+        <ul class = "list-group">
+          <li class="list-group-item"><span class="sender"> <b>${email['sender']}</b> </span>
+          <span class="subject"> ${email['subject']} </span>
+          <span class="timestamp"> ${email['timestamp']} </span> </li>
+        </ul>  
+        `;
+      div.addEventListener('click', () => get_email(email['id']))
+      document.querySelector('#emails-view').append(div);
+    })
+
+    if (!email['read']) {
+      fetch('/emails/' + id, {
+        method: 'PUT',
+        body: JSON.stringify({read:true})
+      })
+      .then(alert('works'))
+    }
+  })
 }
 
 function create_sent(event) {
+  event.preventDefault()
+
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -44,10 +79,30 @@ function create_sent(event) {
         body: document.querySelector('#compose-body').value
     })
   })
-  .then(response => load_mailbox('sent'));
+  .then(result => load_mailbox('sent'));
 }
 
 function get_email(id) {
  fetch('/emails/' + id)
  .then(response => response.json())
+ .then(email => {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'block';
+  const email_view = document.querySelector('#email');
+  email_view.innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item"> <b>From:</b> <span>${email['sender']}</span></li>
+      <li class="list-group-item"> <b>To: </b><span>${email['recipients']}</span></li>
+      <li class="list-group-item"> <b>Subject:</b> <span>${email['subject']}</span</li>
+      <li class="list-group-item"> <b>Time:</b> <span>${email['timestamp']}</span></li>
+    </ul>
+    <p class="m-2">${email['body']}</p>
+        `;
+ })
+ fetch('/emails/' + email['id'], {
+  method : 'PUT',
+  body: JSON.stringify({ archived : !email['archived'] })
+ })
+ .then(response => load_mailbox('inbox'))
 } 
